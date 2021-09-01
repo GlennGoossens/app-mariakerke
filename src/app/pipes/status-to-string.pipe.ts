54e64edb72bf77c5452c97398882878a55c977bb
Glenn Goossens <glenngoossens07@gmail.com>
2021-09-01 23:42:39 +0200

    admin

---
+++ src/app/pipes/status-to-string.pipe.ts
@@ -0,0 +1,20 @@
+import { BookingStatus } from './../models/booking';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'statusToString'
+})
+export class StatusToStringPipe implements PipeTransform {
+
+  transform(value: BookingStatus, ...args: unknown[]): string {
+    switch (value) {
+      case BookingStatus.Free:
+        return "Vrij";
+      case BookingStatus.Reserved:
+        return "Nog te betalen";
+      case BookingStatus.Booked:
+        return "Geboekt";
+    }
+  }
+
+}
